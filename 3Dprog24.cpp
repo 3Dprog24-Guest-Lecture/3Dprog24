1f9372842033eaaeba2950cf0bd6f776637b16f6
AA-A93 <ali.al-awadi@hotmail.com>
2024-02-11 16:45:23 +0100

    SQ3D-4 Initialize OpenGL with cmake
    Added all necessary files for opengl with cmake

---
+++ 3Dprog24.cpp
@@ -0,0 +1,15 @@
+﻿// 3Dprog24.cpp : Defines the entry point for the application.
+//
+
+#include "3Dprog24.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
