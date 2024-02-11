334b905c98d2dfde6423d75b7fe0cafa6b0e6d50
AA-A93 <ali.al-awadi@hotmail.com>
2024-02-11 18:04:07 +0100

    SQ3D-7 Getting Started OpenGL tutorial
    Completed the "getting started" introduction to OpenGL

--- 3Dprog24.cpp
+++ 3Dprog24.cpp
@@ -87,20 +87,6 @@ unsigned int indices[] = {  // note that we start from 0!
     1, 2, 3    // second triangle
 };
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\0";
-
 int main()
 {
     glfwInit();
